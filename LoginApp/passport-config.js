3df2a9cdaa298262c3c20f6e3e4b85e0f79688e1
NikolaCiric7117 <nciric@stevens.edu>
2022-02-12 00:13:03 -0500

    fished base of simple login app. Need to add dependencies and env

--- LoginApp/passport-config.js
+++ LoginApp/passport-config.js
@@ -1,30 +1,29 @@
-const { authenticate } = require("passport")
-const LocalStart = require("passport-local").Strategy
-const bcrypt = require("bcrypt")
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
 
-function initialize(passport, getUserByEmail) {
+function initialize(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
     const user = getUserByEmail(email)
     if (user == null) {
-      return done(null, false, {message:"no user with that email"})
+      return done(null, false, { message: 'No user with that email' })
     }
+
     try {
       if (await bcrypt.compare(password, user.password)) {
-        return done(null, user, )
-        
+        return done(null, user)
       } else {
-        return done(null, false, {message: "password incorrect"})
+        return done(null, false, { message: 'Password incorrect' })
       }
     } catch (e) {
       return done(e)
-      
     }
-
-
   }
-  passport.use(new LocalStart({ usernameField: "email" }), authenticateUser)
-  passport.serializeUser((user, done) => {  })
-  passport.deserializeUser((id, done) => { })
-  
+
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
+  passport.serializeUser((user, done) => done(null, user.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
 }
+
 module.exports = initialize
